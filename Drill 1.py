f04384634c22cc99b6b570559ca08d19d21c06df
HyeonSang Cho <wolf1323@naver.com>
2023-09-15 20:46:37 +0900

    reset 추가

--- Drill 1.py
+++ Drill 1.py
@@ -6,10 +6,14 @@ def drunken_move():
     turtle.forward(random.randint(50,100))
     turtle.stamp()
 
+def restart():
+    turtle.reset()
+
 turtle.shape('turtle')
 
 turtle.onkey(drunken_move, 'w')
 turtle.onkey(drunken_move, 'a')
 turtle.onkey(drunken_move, 's')
 turtle.onkey(drunken_move, 'd')
+turtle.onkey(restart, 'Escape')
 turtle.listen()
